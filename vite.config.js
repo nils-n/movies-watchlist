ed0198c44d43a7eae3286dc33cccd48a41835ada
Nils <52067471+nils-n@users.noreply.github.com>
2024-02-12 10:08:30 +0000

    deployment commit - fix vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
+// vite.config.js
+
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        otherPage: resolve(__dirname, "watchlist.html"),
+      },
+    },
+  },
   plugins: [],
 });
